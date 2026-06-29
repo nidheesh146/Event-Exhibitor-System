fc2d31d67dabca643a0035f9dd9dd1e2a905b485
Nidheesh Ks <nidheeshks0776@gmail.com>
2026-06-29 15:14:10 +0530

    Add STATIC_ROOT for deployment

--- Event_Exhibition/settings.py
+++ Event_Exhibition/settings.py
@@ -124,10 +124,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
-
-
-
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 
 from datetime import timedelta
